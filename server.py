14d7fa8052b6fab058a006b5df2e7ed158a52c61
ricardo <ric91cardoso@gmail.com>
2020-11-21 22:20:00 +0000

    the real game begins now

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
-import subprocess
+import os
 
+SERVICES = ['bind9', 'dhcpd', 'nfs-kernel-server', 'haproxy', 'snmpd', 'tomcat7']
 HOST = '127.0.0.1'
 PORT = 65432
 
@@ -15,3 +16,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             conn.sendall(data)
+
+
+def serviceChecker(name):
+    output = os.system('service ' + name + ' status' + ' >/dev/null 2>&1')
+    return output
+
+
+def getInfo():
+    for i in range(len(SERVICES)):
+        print(SERVICES[i] + ' status: ' + str(serviceChecker(SERVICES[i])))
+
+
